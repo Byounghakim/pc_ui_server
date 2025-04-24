a788a15764b67da73571ef7f818f9f6647e01804
Byounghakim <146217623+Byounghakim@users.noreply.github.com>
2025-04-25 05:43:03 +0900

    수정 2

--- app/config/environment.ts
+++ app/config/environment.ts
@@ -32,7 +32,7 @@ const environmentSchema = z.object({
   ),
   
   // MQTT 서버 설정
-  mqttDevUrl: z.string().default('ws://203.234.35.54:8080'),
+  mqttDevUrl: z.string().default('ws://192.168.0.26:8080'),
   mqttProdUrl: z.string().default('ws://203.234.35.54:8080'),
   mqttUsername: z.string().default('dnature'),
   mqttPassword: z.string().default('8210'),
